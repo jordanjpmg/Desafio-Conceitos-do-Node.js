e6aebf44f275b11d15987b7a12597f71654097ba
jordanjpmg <jordanjpmg@gmail.com>
2020-04-08 19:19:48 -0300

    Challenge Concluded

--- src/app.js
+++ src/app.js
@@ -1,33 +1,63 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
-
+const { uuid } = require("uuidv4");
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
 const repositories = [];
 
+function validaterepo (request, response, next){
+  const { id } = request.params;
+  const repository = repositories.find(repository => repository.id === id);
+  if(!repository){
+    return response.status(400).send();
+  };
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validaterepo, (request, response) => {
+  const {id} = request.params;
+  const { title, url, techs } = request.body;
+  const repository = repositories.find(repository => repository.id === id);
+  repository.url = url;
+  repository.title = title;
+  repository.techs = techs;
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+
+app.delete("/repositories/:id", validaterepo, (request, response) => {
+  const {id} = request.params;
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  repositories.splice(repositorieIndex, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+app.post("/repositories/:id/like", validaterepo,  (request, response) => {
+  const {id} = request.params;
+  const repository = repositories.find(repositorie => repositorie.id === id);
+  repository.likes++;
+  
+  return response.json(repository);
 });
 
 module.exports = app;
